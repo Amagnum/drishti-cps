a727ddc483df715c8bdf3059e9a146f4127e4390
fluorspar20 <amoghdixit104@gmail.com>
2021-01-10 22:36:20 +0530

    opening carousel and news section finished

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
+import Carousel from './Carousel/Carousel'
+import Updates from './NewsAndUpdates/Updates'
+
 import './Home.css'
 
 function Home() {
     return (
         <div className="container home">
             <div className="row">
-                <div className="col-12 col-md-8">
-                    Carousel Here
+                <div className="mb-5 col-12 col-md-8">
+                    <Carousel />
                 </div>
-                <div className="col-12 col-md-4">
-                    Updates Here
+                <div className="mb-5 col-12 col-md-4">
+                    <Updates />
                 </div>
             </div>
+
         </div>
     )
 }
