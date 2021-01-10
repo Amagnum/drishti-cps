f00129ba3b17c513c4b4287dc7aeae9207c46863
fluorspar20 <amoghdixit104@gmail.com>
2021-01-10 22:01:38 +0530

    navbar finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Switch, Route, Redirect, withRouter, BrowserRouter } from 'react-router-dom'
+
+import Header from './Components/Header/Header'
+import Home from './Components/Home/Home'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/about" />
+        <Route path="/programmes" />
+        <Route path="/partners" />
+        <Route path="/portfolio" />
+        <Route path="/connect" />
+        <Route path="/testimonials" />
+        <Redirect to="/home" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="container home">
+            <div className="row">
+                <div className="col-12 col-md-8">
+                    Carousel Here
+                </div>
+                <div className="col-12 col-md-4">
+                    Updates Here
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
