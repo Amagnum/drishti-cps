170c11dac6f73b1271e56bc5c9952f5a6d1ec2ef
Adarsh Baghel <43698929+Amagnum@users.noreply.github.com>
2021-01-13 02:18:26 +0530

    Some changes adas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Switch, Route, Redirect, withRouter, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
+import React from "react";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
 
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
-import Home from './Components/Home/Home'
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
