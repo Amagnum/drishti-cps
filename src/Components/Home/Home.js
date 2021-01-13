247b4ba1fff253eb51ab7bfcb058940e5087bce4
fluorspar20 <amoghdixit104@gmail.com>
2021-01-13 11:10:35 +0530

    about section done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -17,7 +17,18 @@ function Home() {
                     <Updates />
                 </div>
             </div>
+            <div className="row">
+                <div className="row-caption">
+                    <h3>About Drishti CPS</h3>
+                </div>
+                <div className="col-12 col-md-10 m-auto">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque possimus perspiciatis quia nam consequuntur, nemo ratione voluptatem maxime labore et officiis aliquid unde aliquam harum. Quidem cum cupiditate, eius sit eveniet, vitae, rerum harum repudiandae nisi consequuntur fugit labore cumque eaque impedit facilis excepturi voluptatibus corrupti dicta dignissimos? Officiis optio odio dignissimos quis accusamus alias perferendis dolore, suscipit consequuntur. Quas atque obcaecati nobis eligendi officiis vero, quis inventore aut voluptatibus doloremque fuga incidunt dignissimos dolores rem exercitationem dolorem sint odit consequatur eos? Asperiores quas temporibus maxime unde nemo. Est illo sunt quod fuga recusandae dignissimos provident iure, voluptas praesentium non.
+                </div>
+            </div>
             <div className="row row-content">
+                <div className="row-caption">
+                    <h3>Impact</h3>
+                </div>
                 <div className="col-12 col-md-3">
                     <Stats src="assets/startups.svg" caption1="100+" caption2="Startups" />
                 </div>
